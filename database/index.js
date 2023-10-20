e8b48e1f1fd3e12f1c27ee728ec32361e5c61fcd
Alvaro Estevez <alvaro5estevez@gmail.com>
2023-10-20 16:38:38 +0100

    index.js db

---
+++ database/index.js
@@ -0,0 +1,34 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DIALECT,
+    port: process.env.DB_PORT,
+    logging: false
+})
+
+async function checkConnection() {
+    try {
+        await sequelize.authenticate()
+        console.log('Connection to DB has been established successfully')
+
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+async function syncModels(value) {
+    try {
+        const state = {
+            alter: { alter: true },
+            force: { force: true }
+        }
+        await sequelize.sync(state[value] || "")
+        console.log('Models has been synchronized successfully')
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+
+Module.exports = { sequelize,checkConnection, syncModels }
