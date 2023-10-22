25426172ee76e02d1fda534d29342cf57b9cfcc3
SaraBetancorLeón <141911161+sabele90@users.noreply.github.com>
2023-10-22 19:43:32 +0100

    classroom.model

---
+++ api/models/classroom.model.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize')
+const { sequelize } = require('../../database')
+
+const ClassRoom = sequelize.define(
+    'classroom',
+    {
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		floor: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        
+        capacity: {
+			type: DataTypes.INTEGER,
+        },
+        
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: function () {
+                return new Date()
+            }
+		},
+        updatedAt: {
+            type: DataTypes.DATE,
+            defaultValue: function () {
+                return new Date()
+            }
+		}
+    },
+)
