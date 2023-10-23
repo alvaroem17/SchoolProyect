8a13eed6ce8eef31bf356482eefca72823323373
jGilarana <jorgereboot@gmail.com>
2023-10-23 11:48:39 +0100

    studentDone

--- api/routers/index.js
+++ api/routers/index.js
@@ -2,4 +2,5 @@ const router = require('express').Router()
 
 router.use('/employees', require('./employee.router.js'))
 router.use('/inventories', require('./inventory.router.js'))
+router.use('/students', require('./student.router.js'))
 module.exports=router
