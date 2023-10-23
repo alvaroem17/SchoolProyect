8a79a5ec0dc88cde4b2caedf97ea4810a8eae5bf
jGilarana <jorgereboot@gmail.com>
2023-10-23 15:17:58 +0100

    all CRUD done

--- api/routers/contactInfo.router.js
+++ api/routers/contactInfo.router.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 
+// /contactInfo/:contactInfoId/student
+
 const { getAllContactInfos,getOneContactInfo, updateContactInfo, createContactInfo, deleteContactInfo} = require('../controllers/contactInfo.controller')
 
 router.get('/', getAllContactInfos)
@@ -7,6 +9,7 @@ router.get('/:id', getOneContactInfo)
 router.post('/', createContactInfo)
 router.put('/:id', updateContactInfo)
 router.delete('/:id', deleteContactInfo)
+
 
 
 module.exports=router
