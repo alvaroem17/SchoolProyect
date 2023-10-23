65ec249fa2276e66422118d831ffa01abf704004
Alvaro Estévez <alvaro5estevez@gmail.com>
2023-10-23 11:56:32 +0100

    Roles controller, model and router

--- api/routers/index.js
+++ api/routers/index.js
@@ -2,4 +2,6 @@ const router = require('express').Router()
 
 router.use('/employees', require('./employee.router.js'))
 router.use('/inventories', require('./inventory.router.js'))
+router.use('/roles', require('./roles.router.js'))
+
 module.exports=router
--- database/model.js
+++ database/model.js
@@ -1,6 +1,7 @@
 const Employee = require('../api/models/employee.model')
 const ClassRoom = require('../api/models/classroom.model')
 const Inventory = require('../api/models/inventory.model')
+//const Roles = require('../api/models/role.model')
 
 function addRelationsToModels() {
 	try {
