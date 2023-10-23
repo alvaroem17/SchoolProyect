ff021f999d470a850c4f5905a207693a2e3621a9
sabele90 <sabele@hotmail.es>
2023-10-23 11:39:44 +0100

    classroom done

--- api/routers/index.js
+++ api/routers/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 
 router.use('/employees', require('./employee.router.js'))
+router.use('/classrooms',require('./classroom.router.js'))
 
 module.exports=router
